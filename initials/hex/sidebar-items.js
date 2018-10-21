f5a2344c95742352a40a1b8cf974e82efda566fe
Travis CI User <travis@example.org>
2018-10-21 20:04:53 +0000

    Update documentation

---
+++ initials/hex/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["parse_hex","Parse hex code and generate RGB vector accordingly."]]});
---
+++ initials/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"mod":[["avatar","Avatar module helps to generate avatars according to the initial names."],["contrast","Contrast module helps to calculate constrast ratio of two RGB color."],["error","Error module includes the custom error types."],["hex","Hex module helps to convert Color Hex Code to RGB."]]});
